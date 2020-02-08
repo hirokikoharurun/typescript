88fd3be2682900d347a0dc1935ade5d1d80c64da
hiroki <hirokikoharu20180530@gmail.com>
2020-02-09 00:00:50 +0900

    commit2

--- index.ts
+++ index.ts
@@ -34,3 +34,22 @@ const coffee = {
 
 //any型　なんでも入る
 let anything = true;
+
+//Union型 数字も文字列もいれたい時に使う
+let unionType: number | string = 10;
+
+//Literal型 特定の値のみを扱う
+const apple: 'apple' = 'apple';
+
+function add( num1: number,num2:number ):number{
+    return num1 + num2;
+}
+
+add(2,3);
+
+function sayHello(){
+    console.log('hello!');
+}
+
+const doubleNumber = ( number: number ):number => number * 2 ;
+
